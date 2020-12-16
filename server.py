d22ac86c72f13d60a73f2468bbf0220597177f5a
Mihai <mihaibot_93@yahoo.com>
2020-12-16 23:55:47 +0200

    add recommendations

--- server.py
+++ server.py
@@ -2,124 +2,92 @@ from flask import Flask, jsonify, json, request, Response, render_template, redi
 from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
-from .model.model import  ClothesDetectionModel
+from model.model import ClothesDetectionModel
 import json
 
 category_to_id = {
-	"short sleeve top":		1,
-	"long sleeve top":		2,
-	"short sleeve outwear":	4,
-	"long sleeve outwear":	4,
-	"vest":					4,
-	"sling":				6,
-	"shorts":				8,
-	"trousers":				8,
-	"skirt":				9,
-	"short sleeve dress":	10,
-	"long sleeve dress":	10,
-	"vest dress":			10,
-	"sling dress":			10
+	"short sleeve top": 1,
+	"long sleeve top": 2,
+	"short sleeve outwear": 4,
+	"long sleeve outwear": 4,
+	"vest": 4,
+	"sling": 6,
+	"shorts": 8,
+	"trousers": 8,
+	"skirt": 9,
+	"short sleeve dress": 10,
+	"long sleeve dress": 10,
+	"vest dress": 10,
+	"sling dress": 10
 }
 
-items = {}
-with open("filtered_clothes.json" , 'r') as outfile:
-    items = json.load(outfile)
 
-def get_recommendation(predicted_items, max_items = 3, max_pictures = 3):
+def get_recommendation(predicted_items, max_items=3, max_pictures=3):
 	recommendations = []
-	
+
 	for pred in predicted_items:
 		id = category_to_id[pred]
 		recommendations_count = max_items
-		
+
+		items = {}
+		with open("filtered_clothes.json", 'r') as outfile:
+			items = json.load(outfile)
+
 		for item in items:
 			if item["category_id"] == id:
-				recommendations += item["images"][0:max_pictures]
+				_item = item["images"][0]
+				_page = item["page"]
+
+				recommendations.append({"image": _item, "url": _page})
+
 				recommendations_count -= 1
 
 			if recommendations_count <= 0:
 				break
-	
+
 	return recommendations
 
-rec = get_recommendation(["sling dress", "vest"],1,1)
-
-print(rec)
 
 app = Flask(__name__)
 detection_model = ClothesDetectionModel(r"D:\master\an2\SRI\project\clothes_recognition_and_retrieving")
 
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files.get('file', '')
+	file = request.files.get('file', '')
 
-    if file:
-        filename = secure_filename(file.filename)
-        filename, extension = os.path.splitext(filename)
-        filename = filename + datetime.now().strftime("%d_%m_%Y %H_%M_%S") + extension
+	if file:
+		filename = secure_filename(file.filename)
+		filename, extension = os.path.splitext(filename)
+		filename = filename + datetime.now().strftime("%d_%m_%Y %H_%M_%S") + extension
 
-        file_path = os.path.join('static/uploaded_images', filename)
+		file_path = os.path.join('static/uploaded_images', filename)
 
-        file.save(file_path)
+		file.save(file_path)
 
-        detection_model.get_image_label(file_path)
+		return jsonify({'filename': filename})
 
-        return jsonify({'filename': filename})
-
-    return render_template('home.html', response1=False, upload_image=None)
+	return render_template('home.html', response1=False, upload_image=None)
 
 
 @app.route("/", methods=['GET'])
 def main():
-    filename = request.args.get('filename', '')
-    items = None
-    upload_image = None
+	filename = request.args.get('filename', '')
+	upload_image = None
+	items = []
+	if filename:
+		file_path = 'uploaded_images/' + filename
+		upload_image = url_for('static', filename=file_path)
+		results = detection_model.get_image_label(os.path.join('static', file_path))
+		print(results)
 
-    if filename:
-        upload_image = url_for('static', filename='uploaded_images/' + filename)
-        items = [
-            {
-                'image': 'https://images-na.ssl-images-amazon.com/images/I/610irNyucGL._UL1500_.jpg',
-                'url': 'https://www.cleany.ro',
-            },
-            {
-                'image': 'https://www.sunspel.com/media/catalog/product/cache/bee6a030eca197d5b3b98b85dbca461b/m/t/mtsh0001-gyab_1_1.jpg',
-                'url': 'https://www.cleany.ro',
-            }
-        ]
+		_results = [res for res in results.keys() if results[res] > 0.7]
+		print(_results)
 
-    return render_template('home.html', items=items, upload_image=upload_image)
+		items = get_recommendation(_results)
+
+	return render_template('home.html', items=items, upload_image=upload_image)
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8001, debug=True)
-
-
-# initial_category_to_id = {
-	# "short sleeve top":1,
-	# "long sleeve top":2,
-	# "short sleeve outwear":3,
-	# "long sleeve outwear":4,
-	# "vest":5,
-	# "sling":6,
-	# "shorts":7,
-	# "trousers":8,
-	# "skirt":9,
-	# "short sleeve dress":10,
-	# "long sleeve dress":11,
-	# "vest dress":12,
-	# "sling dress":13
-# }
-
-# categories = {
-    # "fuste" : 9,
-    # "rochii" : 10,
-    # "tricouri" : 1,
-    # "pantaloni" : 8,
-    # "geci": 4,
-    # "curele":6,
-    # "camasi": 2,
-    # "bluze":2,
-    # "sacouri":2,
-    # "pulovere":2
-# }
+	app.run(host='127.0.0.1', port=8001, debug=True)

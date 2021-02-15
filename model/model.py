f44b3e2e405b107d5d173510966210b73f204488
Mihai <mihaibot_93@yahoo.com>
2021-02-15 19:59:33 +0200

    update with hash similarities

--- model/model.py
+++ model/model.py
@@ -11,6 +11,7 @@ import numpy as np
 from collections import Counter
 
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+import json
 
 
 class ToTensor(object):
@@ -30,9 +31,7 @@ class Compose(object):
 
 
 def get_transform():
-	transforms = []
-	transforms.append(ToTensor())
-	return Compose(transforms)
+	return Compose([ToTensor()])
 
 
 num_classes = 13
@@ -66,7 +65,21 @@ class ClothesDetectionModel(object):
 
 		self.model.to(self.device)
 
+		self.activation = {}
+
 		self.n_clusters = 3
+
+	def get_activation(self, name):
+		def hook(model, input, output):
+			if name not in self.activation.keys():
+				self.activation[name] = [output.detach()]
+			else:
+				self.activation[name].append(output.detach())
+
+		# print("activation name", self.activation[name])
+		# print("activation", self.activation)
+
+		return hook
 
 	def dominant_colors(self, img):
 		# convert to rgb from bgr
@@ -93,22 +106,32 @@ class ClothesDetectionModel(object):
 
 		return COLORS.astype(int)[max_cluster]
 
-	def dominant_box_color(self, img_path):
+	def dominant_box_color(self, img_path, bad_converts=None):
 		img = cv2.imread(img_path)
-
 		boxes = self.get_image_boxes(img)
 
 		# for box_key in boxes:
 		# 	box = boxes[box_key].astype(int)
 
-		box = boxes[list(boxes.keys())[0]].astype(int)
-		_img = img[box[0]: box[2], box[1]: box[3]]
+		if len(boxes) < 1:
+			if bad_converts:
+				bad_converts[0] += 1
+			return []
 
 		# cv2.imshow('image', _img)
 		# cv2.waitKey(0)
 
-		color = self.dominant_colors(_img)
-		print("color: ", color)
+		box = boxes[list(boxes.keys())[0]].astype(int)
+
+		if img.shape[0] <= box[0] or img.shape[1] <= box[1]:
+			if bad_converts:
+				bad_converts[0] += 1
+			return []
+		else:
+			_img = img[box[0]: box[2], box[1]: box[3]]
+
+		color = [int(color) for color in self.dominant_colors(_img)]
+
 		return color
 
 	def color_dist(self, color1, color2):
@@ -128,6 +151,8 @@ class ClothesDetectionModel(object):
 		scores = prediction[0]['scores'].cpu().numpy()
 
 		for idx in range(3):
+			if idx >= len(labels):
+				break
 			result[categories[labels[idx]]] = scores[idx]
 
 		return result
@@ -146,12 +171,55 @@ class ClothesDetectionModel(object):
 		scores = prediction[0]['scores'].cpu().numpy()
 
 		for idx in range(3):
+			if idx >= len(scores):
+				break
 			if scores[idx] > 0.7:
 				result[categories[labels[idx]]] = boxes[idx]
 
 		return result
 
+	def vector_distance(self, x1, x2):
+		return np.linalg.norm(np.asarray(x1) - np.asarray(x2))
+
+	def get_vector(self):
+		return torch.sum(self.activation['fc6'][-1], dim=0).tolist()
+
+	def init_hook(self):
+		i = 0
+		for child in self.model.roi_heads.children():
+			if i == 1:
+				for layer in child.children():
+					layer.register_forward_hook(self.get_activation('fc6'))
+					break
+			i += 1
+
+
+def save_image_vector():
+	detection_model = ClothesDetectionModel(r"D:\master\an2\SRI\project\clothes_recognition_and_retrieving")
+	detection_model.init_hook()
+
+	with open("data/all_clothes.json", 'r') as outfile:
+		items = json.load(outfile)
+
+	index = 0
+	for item in items:
+		index += 1
+		if index <= 1000:
+			continue
+
+		image_path = r"" + item['local_images'][0]
+		if os.path.exists(image_path):
+			detection_model.get_image_label(image_path)
+			item['vector'] = torch.sum(detection_model.activation['fc6'][-1], dim=0).tolist()
+
+		if index % 10 == 0:
+			print(index)
+
+	with open("data/all_clothes.json", 'w') as outfile:
+		json.dump(items, outfile)
+
+# save_image_vector()
+
 # if __name__ == '__main__':
-# 	model = ClothesDetectionModel(r"D:\master\an2\SRI\project\clothes_recognition_and_retrieving")
 # 	path = os.path.join(r"D:\master\an2\SRI\project\clothes_recognition_and_retrieving\data\deepfashion\train\image", "000011.jpg")
 # 	print(model.get_image_label(path))
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, json, request, Response, render_template, redi
 from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
-from model.model import ClothesDetectionModel
+from model.model import ClothesDetectionModel, save_image_vector
 import json
 
 category_to_id = {
@@ -22,34 +22,59 @@ category_to_id = {
 }
 
 
-def get_recommendation(predicted_items, max_items=3, max_pictures=3):
-	recommendations = []
-
-	for pred in predicted_items:
-		id = category_to_id[pred]
-		recommendations_count = max_items
-
-		items = {}
-		with open("filtered_clothes.json", 'r') as outfile:
-			items = json.load(outfile)
-
-		for item in items:
-			if item["category_id"] == id:
-				_item = item["images"][0]
-				_page = item["page"]
-
-				recommendations.append({"image": _item, "url": _page})
-
-				recommendations_count -= 1
-
-			if recommendations_count <= 0:
-				break
-
-	return recommendations
-
-
 app = Flask(__name__)
 detection_model = ClothesDetectionModel(r"D:\master\an2\SRI\project\clothes_recognition_and_retrieving")
+detection_model.init_hook()
+
+def get_recommendation(predicted_items, main_color, main_vector, is_color, max_items=10):
+	recommendations = []
+
+	items = {}
+	with open("data/all_clothes.json", 'r') as outfile:
+		items = json.load(outfile)
+
+	dists = []
+	print(category_to_id[predicted_items[0]])
+	for item in items:
+		if item["category_id"] == category_to_id[predicted_items[0]]:
+			_item = item["images"][0]
+			_page = item["page"]
+
+			if is_color:
+				if len(item['rgb_color']) != 3:
+					continue
+				dists.append(detection_model.color_dist(main_color, item['rgb_color']))
+			else:
+				if 'vector' not in item.keys():
+					continue
+				dists.append(detection_model.vector_distance(main_vector, item['vector']))
+
+			recommendations.append({"image": _item, "url": _page, "price": str(item['lei']) + " RON"})
+
+	top_recommandations = []
+	current_recomm_vector = dists
+	while max_items:
+		min_dist = min(current_recomm_vector)
+		print(min_dist)
+		index = current_recomm_vector.index(min_dist)
+
+		top_recommandations.append(recommendations[index])
+		recommendations.pop(index)
+		current_recomm_vector.pop(index)
+
+		max_items -= 1
+
+	print(top_recommandations)
+	return top_recommandations
+
+
+
+def get_files_name(dir_name='data/images'):
+	paths = []
+	for path in os.listdir(dir_name):
+		paths.append(dir_name + "/" + path)
+
+	return paths
 
 
 @app.route('/upload', methods=['POST'])
@@ -73,8 +98,12 @@ def upload():
 @app.route("/", methods=['GET'])
 def main():
 	filename = request.args.get('filename', '')
+	is_color = request.args.get('is_color', True)
+	if is_color == 'false':
+		is_color = False
+
 	upload_image = None
-	items = []
+	items = None
 	if filename:
 		file_path = 'uploaded_images/' + filename
 		upload_image = url_for('static', filename=file_path)
@@ -83,19 +112,46 @@ def main():
 
 		# return a dictionary with labels and scores
 		results = detection_model.get_image_label(os.path.join('static', file_path))
+		vector = detection_model.get_vector()
+
+		max_score = max(results.values())
+		if max_score <= 0.7:
+			results = []
+		else:
+			key = list(results.keys())[list(results.values()).index(max_score)]
+			results = [key]
 
 		# return the dominant color of the first box
-		color = detection_model.dominant_box_color(img_path)
+		main_color = detection_model.dominant_box_color(img_path)
 
-		print(results, color)
-
-		_results = [res for res in results.keys() if results[res] > 0.7]
-		print(_results)
-
-		items = get_recommendation(_results)
+		items = get_recommendation(results, main_color, vector, is_color)
 
 	return render_template('home.html', items=items, upload_image=upload_image)
 
 
+def save_image_color():
+	with open("data/all_clothes.json", 'r') as outfile:
+		items = json.load(outfile)
+
+	index = 0
+	bad_values = [0]
+	for item in items:
+		if index % 10 == 0:
+			print(index)
+		index += 1
+
+		image_path = r"" + item['local_images'][0]
+		if os.path.exists(image_path):
+			item['rgb_color'] = list(detection_model.dominant_box_color(image_path, bad_values))
+		else:
+			print("no_image")
+
+	print("bad_values", bad_values)
+
+	with open("data/all_clothes.json", 'w') as outfile:
+		json.dump(items, outfile)
+
+
 if __name__ == "__main__":
 	app.run(host='127.0.0.1', port=8001, debug=True)
+
